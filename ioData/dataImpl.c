fc326e3aef82fdbe84e1706fa1762fe0b6ea77a2
fmandracci <fmandracci@mect.it>
2016-01-19 19:01:46 +0100

    Relax the timings for TCP_SRV.

--- ioData/dataImpl.c
+++ ioData/dataImpl.c
@@ -2250,105 +2250,112 @@ static void *serverThread(void *arg)
     // run
     theServers[s].thread_status = RUNNING;
     while (!engineExiting) {
-        if (engineRunning && threadInitOK) {
-            // get file descriptor or bind and listen
-            if (server_socket == -1) {
-                switch (theServers[s].protocol) {
-                case RTU_SRV:
-                    server_socket = modbus_get_socket(modbus_ctx); // here socket is file descriptor
-                    break;
-                case TCP_SRV:
-                    server_socket = modbus_tcp_listen(modbus_ctx, THE_SRV_MAX_CLIENTS);
-                    break;
-                case TCPRTU_SRV:
-                    server_socket = modbus_tcprtu_listen(modbus_ctx, THE_SRV_MAX_CLIENTS);
-                    break;
-                default:
-                    ;
-                }
-                if (server_socket >= 0) {
-                    FD_ZERO(&refset);
-                    FD_SET(server_socket, &refset);
-                    fdmax = server_socket;
-                } else {
-                    do_sleep_ms(THE_SERVER_DELAY_ms);
-                    continue;
-                }
-            }
-            // wait on server socket, only until timeout
-            struct timeval tv;
-            rdset = refset;
-            tv.tv_sec = THE_SERVER_DELAY_ms / 1000;
-            tv.tv_usec = THE_SERVER_DELAY_ms % 1000;
-            if (select(fdmax+1, &rdset, NULL, NULL, &tv) <= 0) {
-                // timeout or error
-                continue;
-            }
-            // accept requests
+
+        // trivial scenario
+        if (!engineRunning || !threadInitOK) {
+            do_sleep_ms(THE_CONNECTION_DELAY_ms);
+            continue;
+        }
+
+        // get file descriptor or bind and listen
+        if (server_socket == -1) {
             switch (theServers[s].protocol) {
             case RTU_SRV:
-                // unique client (serial line)
-                rc = modbus_receive(modbus_ctx, query);
-                if (rc > 0) {
-                    pthread_mutex_lock(&theServers[s].mutex);
-                    {
-                        modbus_reply(modbus_ctx, query, rc, theServers[s].mb_mapping);
-                    }
-                    pthread_mutex_unlock(&theServers[s].mutex);
-                }
+                server_socket = modbus_get_socket(modbus_ctx); // here socket is file descriptor
                 break;
             case TCP_SRV:
+                server_socket = modbus_tcp_listen(modbus_ctx, THE_SRV_MAX_CLIENTS);
+                break;
             case TCPRTU_SRV:
-                // multiple clients (tcp/ip server)
-                for (master_socket = 0; master_socket <= fdmax; ++master_socket) {
-                    if (!FD_ISSET(master_socket, &rdset)) {
-                        continue;
-                    }
-                    if (master_socket == server_socket) {
-                        socklen_t addrlen;
-                        struct sockaddr_in clientaddr;
-                        int newfd;
-
-                        addrlen = sizeof(clientaddr);
-                        memset(&clientaddr, 0, sizeof(clientaddr));
-                        newfd = accept(server_socket, (struct sockaddr *)&clientaddr, &addrlen);
-                        if (newfd == -1) {
-                            perror("Server accept() error");
-                        } else {
-                            FD_SET(newfd, &refset);
-
-                            if (newfd > fdmax) {
-                                fdmax = newfd;
-                            }
-                                printf("New connection from %s:%d on socket %d\n",
-                                    inet_ntoa(clientaddr.sin_addr), clientaddr.sin_port, newfd);
-                        }
-                    } else {
-                        modbus_set_socket(modbus_ctx, master_socket);
-                        rc = modbus_receive(modbus_ctx, query);
-                        if (rc > 0) {
-                            pthread_mutex_lock(&theServers[s].mutex);
-                            {
-                                modbus_reply(modbus_ctx, query, rc, theServers[s].mb_mapping);
-                            }
-                            pthread_mutex_unlock(&theServers[s].mutex);
-                        } else if (rc == -1) {
-                            printf("Connection closed on socket %d\n", master_socket);
-                            close(master_socket);
-
-                            FD_CLR(master_socket, &refset);
-                            if (master_socket == fdmax) {
-                                --fdmax;
-                            }
-                        }
-                    }
-                }
+                server_socket = modbus_tcprtu_listen(modbus_ctx, THE_SRV_MAX_CLIENTS);
                 break;
             default:
                 ;
             }
-        } else {
+            if (server_socket >= 0) {
+                FD_ZERO(&refset);
+                FD_SET(server_socket, &refset);
+                fdmax = server_socket;
+            }
+        }
+        if (server_socket < 0) {
             do_sleep_ms(THE_SERVER_DELAY_ms);
+            continue;
+        }
+
+        // wait on server socket, only until timeout
+        struct timeval tv;
+        rdset = refset;
+        tv.tv_sec = THE_SERVER_DELAY_ms / 1000;
+        tv.tv_usec = THE_SERVER_DELAY_ms % 1000;
+        if (select(fdmax+1, &rdset, NULL, NULL, &tv) <= 0) {
+            // timeout or error
+            do_sleep_ms(THE_SERVER_DELAY_ms);
+            continue;
+        }
+
+        // accept requests
+        switch (theServers[s].protocol) {
+        case RTU_SRV:
+            // unique client (serial line)
+            rc = modbus_receive(modbus_ctx, query);
+            if (rc > 0) {
+                pthread_mutex_lock(&theServers[s].mutex);
+                {
+                    modbus_reply(modbus_ctx, query, rc, theServers[s].mb_mapping);
+                }
+                pthread_mutex_unlock(&theServers[s].mutex);
+            }
+            break;
+        case TCP_SRV:
+        case TCPRTU_SRV:
+            // multiple clients (tcp/ip server)
+            for (master_socket = 0; master_socket <= fdmax; ++master_socket) {
+                if (!FD_ISSET(master_socket, &rdset)) {
+                    continue;
+                }
+                if (master_socket == server_socket) {
+                    socklen_t addrlen;
+                    struct sockaddr_in clientaddr;
+                    int newfd;
+
+                    addrlen = sizeof(clientaddr);
+                    memset(&clientaddr, 0, sizeof(clientaddr));
+                    newfd = accept(server_socket, (struct sockaddr *)&clientaddr, &addrlen);
+                    if (newfd == -1) {
+                        perror("Server accept() error");
+                    } else {
+                        FD_SET(newfd, &refset);
+
+                        if (newfd > fdmax) {
+                            fdmax = newfd;
+                        }
+                        fprintf(stderr, "New connection from %s:%d on socket %d\n",
+                            inet_ntoa(clientaddr.sin_addr), clientaddr.sin_port, newfd);
+                    }
+                } else {
+                    modbus_set_socket(modbus_ctx, master_socket);
+                    rc = modbus_receive(modbus_ctx, query);
+                    if (rc > 0) {
+                        pthread_mutex_lock(&theServers[s].mutex);
+                        {
+                            modbus_reply(modbus_ctx, query, rc, theServers[s].mb_mapping);
+                        }
+                        pthread_mutex_unlock(&theServers[s].mutex);
+                    } else if (rc == -1) {
+                        fprintf(stderr, "Connection closed on socket %d\n", master_socket);
+                        close(master_socket);
+
+                        FD_CLR(master_socket, &refset);
+                        if (master_socket == fdmax) {
+                            --fdmax;
+                        }
+                    }
+                }
+            }
+            break;
+        default:
+            ;
         }
     }
 
