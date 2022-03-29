f69a85d23f7821926ce519180f390f5896abc9c9
Andrea Sansoe <asansoe@mect.it>
2022-03-29 11:33:30 +0200

    Version to 2.027

--- ioData/dataImpl.c
+++ ioData/dataImpl.c
@@ -57,7 +57,7 @@
 #define TIMESPEC_FROM_RTIME(ts, rt) { ts.tv_sec = rt / UN_MILIARDO_ULL; ts.tv_nsec = rt % UN_MILIARDO_ULL; }
 
 #define REVISION_HI  2
-#define REVISION_LO  26
+#define REVISION_LO  27
 
 #if DEBUG
 #undef VERBOSE_DEBUG
